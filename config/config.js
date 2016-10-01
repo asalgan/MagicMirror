98b3885a3d3312608b3f476e8d3d5b64c77e6de5
Alan Salganik <alan@babybin.com>
2016-10-01 01:45:50 -0500

    js

---
+++ config/config.js
@@ -0,0 +1,76 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+	language: 'en',
+	timeFormat: 24,
+	units: 'imperial',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+		{
+			module: 'calendar',
+			header: 'US Holidays',
+			position: 'top_left',
+			config: {
+				calendars: [
+					{
+						symbol: 'calendar-check-o',
+						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+					}
+				]
+			}
+		},
+		{
+			module: 'compliments',
+			position: 'lower_third'
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'Chicago',
+				// locationID: 4887398,  //ID from http://www.openweathermap.org
+				appid: '8ec9aa9080c4456a1ab5da3e11342343'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+	      location: 'Chicago',
+				// locationID: '4887398',  //ID from http://www.openweathermap.org
+	      appid: '8ec9aa9080c4456a1ab5da3e11342343'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: 'New York Times',
+						url: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
